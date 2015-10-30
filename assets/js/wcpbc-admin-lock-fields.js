a9aaa0b9f5c84ee3e9b6246cb7274f72e4b8bfda
oscargare <oscargare@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-30 11:39:41 +0000

    fix lock fileds for variable products
    git-svn-id: http://plugins.svn.wordpress.org/woocommerce-product-price-based-on-countries/trunk@1276264 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/wcpbc-admin-lock-fields.js
+++ assets/js/wcpbc-admin-lock-fields.js
@@ -5,8 +5,8 @@ jQuery(document).ready(function($){
 
 		lock_input : function(input_name, disable_attr ) {		
 			for(var i = 0; i<wcpbc_regions_keys.length; i++ ) {	
-				$('input[name="_'+wcpbc_regions_keys[i]+input_name+'"]').attr(disable_attr, disable_attr);
-	        	$('input[name="_'+wcpbc_regions_keys[i]+input_name+'"]').after($('.wcml_lock_img').clone().removeClass('wcml_lock_img').show());
+				$('input[name^="_'+wcpbc_regions_keys[i]+input_name+'"]').attr(disable_attr, disable_attr);
+	        	$('input[name^="_'+wcpbc_regions_keys[i]+input_name+'"]').after($('.wcml_lock_img').clone().removeClass('wcml_lock_img').show());
 	        }
 		},
 
@@ -15,12 +15,14 @@ jQuery(document).ready(function($){
 			this.lock_input('_sale_price', 'readonly');
 			this.lock_input('_price_method', 'disabled');
 
-			$( '#woocommerce-product-data' ).on( 'woocommerce_variations_loaded', function(){
-				//this.lock_input('_variable_regular_price', 'readonly');
-				//this.lock_input('_variable_sale_price', 'readonly');
-				//this.lock_input('_variable_price_method', 'disabled');				
-				alert('aa');
-			} );
+			var that = this;
+
+			$( '#woocommerce-product-data' ).on( 'woocommerce_variations_loaded', function() {
+				that.lock_input('_variable_regular_price', 'readonly');
+				that.lock_input('_variable_sale_price', 'readonly');
+				that.lock_input('_variable_price_method', 'disabled');
+			});
+			
 		}
 	};
 
